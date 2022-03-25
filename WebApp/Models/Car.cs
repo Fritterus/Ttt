bc850d6ffbc2094633c39f4515d806563a856a56
Fritterus <62291430+Fritterus@users.noreply.github.com>
2022-03-25 16:27:38 +0300

    Add class Car

---
+++ WebApp/Models/Car.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApp.Interfaces;
+
+namespace WebApp.Model
+{
+    internal class Car : ICar
+    {
+        public void Move()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
